3f6e4bfe8e674f37da4eb0132b489d5cbacd9567
Julen Cruz <julencruzg@gmail.com>
2026-03-29 23:26:28 +0200

    Setup project

---
+++ src/test/kotlin/com/houseclash/backend/BackendApplicationTests.kt
@@ -0,0 +1,20 @@
+package com.houseclash.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
+
+@SpringBootTest
+@TestPropertySource(properties = [
+	"spring.autoconfigure.exclude=" +
+			"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+			"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+			"org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+])
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
